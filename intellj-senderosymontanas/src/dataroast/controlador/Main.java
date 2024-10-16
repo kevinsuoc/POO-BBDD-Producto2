4eb2071f692db5971b2ca44097fcf9507b8ceffb
kevin seligmann <kseligmann@uoc.edu>
2024-10-16 20:20:12 +0200

    Ahora se pueden agregar excursiones

--- intellj-senderosymontanas/src/dataroast/controlador/Main.java
+++ intellj-senderosymontanas/src/dataroast/controlador/Main.java
@@ -1,10 +1,5 @@
 package dataroast.controlador;
 
-import dataroast.modelo.Datos;
-import dataroast.vista.View;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
         Controlador controlador = new Controlador();
