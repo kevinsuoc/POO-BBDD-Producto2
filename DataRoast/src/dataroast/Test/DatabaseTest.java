43ea8245ec392e3826e0a60b3b763796fcb1a32d
kevin seligmann <kseligmann@uoc.edu>
2024-11-04 18:16:41 +0100

    "Agregado test de conexion más especifico"

--- DataRoast/src/dataroast/Test/DatabaseTest.java
+++ DataRoast/src/dataroast/Test/DatabaseTest.java
@@ -4,14 +4,37 @@ import dataroast.util.MysqlConnection;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabaseTest {
 
     @Test
     void testDatabase(){
+        String codigo = "abc";
+        String nombre = "Una federacion de prueba";
+        String selectFederacionQuery = "SELECT * FROM federacion WHERE codigo = '" + codigo + "' ;";
+        String addFederacionQuery = "INSERT INTO federacion VALUES ('"+ codigo  +"', '"+ nombre +"');";
+
         try {
             Connection connection = MysqlConnection.getConnection();
+
+            connection.setAutoCommit(false);
+
+            PreparedStatement stm = connection.prepareStatement(addFederacionQuery);
+            stm.executeUpdate();
+
+            stm = connection.prepareStatement(selectFederacionQuery);
+            ResultSet results = stm.executeQuery();
+            if (results.next()){
+                assert results.getString("codigo").equals(codigo): "Codigo incorrecto";
+                assert results.getString("nombre").equals(nombre): "Nombre incorrecto";
+            } else {
+                throw new AssertionError("No se ha podido insertar y seleccionar a la base de datos");
+            }
+
+            connection.rollback();
         } catch (SQLException e){
             throw new AssertionError("Database error: " + e);
         }
