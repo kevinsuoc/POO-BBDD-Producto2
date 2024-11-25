e1ebef3d1be3292b14af0a98d197d7c4e40207ab
kevin seligmann <kseligmann@uoc.edu>
2024-11-25 21:26:35 +0100

    Cancelado logging de hibernate para no obstruir la consola. Agregado maven-shade para crear artifacts

--- DataRoast/src/main/java/controlador/Main.java
+++ DataRoast/src/main/java/controlador/Main.java
@@ -2,9 +2,12 @@ package controlador;
 
 import util.HibernateUtil;
 import java.util.Scanner;
+import java.util.logging.Level;
 
 public class Main {
     public static void main(String[] args) {
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+
         Scanner in = new Scanner(System.in);
 
         try {
